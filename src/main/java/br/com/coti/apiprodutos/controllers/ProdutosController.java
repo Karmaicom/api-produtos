4056b2da50606a3cb67f06d8be455abbfdcb0faf
Karmaicom Rocha <karmaicom@gmail.com>
2026-08-21 18:25:12 -0300

    Implementação dos endpoints de Produtos.

--- src/main/java/br/com/coti/apiprodutos/controllers/ProdutosController.java
+++ src/main/java/br/com/coti/apiprodutos/controllers/ProdutosController.java
@@ -1,10 +1,10 @@
 package br.com.coti.apiprodutos.controllers;
 
+import br.com.coti.apiprodutos.entities.dtos.ProdutoAtualizarRequestDto;
+import br.com.coti.apiprodutos.entities.dtos.ProdutoRequestDto;
 import br.com.coti.apiprodutos.entities.mappers.ProdutoMapper;
 import br.com.coti.apiprodutos.exceptions.CreateProdutoException;
-import br.com.coti.apiprodutos.entities.Produto;
-import br.com.coti.apiprodutos.entities.dtos.ProdutoRequestDto;
-import br.com.coti.apiprodutos.factories.ConnectionFactory;
+import br.com.coti.apiprodutos.repositories.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,30 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ProdutosController {
 
-    private final ConnectionFactory connectionFactory;
+    private final ProdutoRepository produtoRepository;
     private final ProdutoMapper produtoMapper;
 
     @PostMapping("criar")
-    public ResponseEntity<?> criar(@RequestBody ProdutoRequestDto dto) {
+    public ResponseEntity<?> inserir(@RequestBody ProdutoRequestDto dto) {
         try {
             var produto = produtoMapper.toProduto(dto);
+            produtoRepository.inserir(produto);
 
-            var query = """
-                    insert into produtos (nome, descricao, preco, quantidade)
-                    values (?, ?, ?, ?)
-                """;
-
-            try (var connection = connectionFactory.getConnection()) {
-                var prepareStatement = connection.prepareStatement(query);
-                prepareStatement.setString(1, produto.getNome());
-                prepareStatement.setString(2, produto.getDescricao());
-                prepareStatement.setDouble(3, produto.getPreco());
-                prepareStatement.setInt(4, produto.getQuantidade());
-
-                prepareStatement.execute();
-
-                return ResponseEntity.status(HttpStatus.OK).body("Produto '" + produto.getNome() + "' criado com sucesso!");
-            }
+            return ResponseEntity.status(HttpStatus.OK).body("Produto '" + produto.getNome() + "' criado com sucesso!");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw new CreateProdutoException("Erro na execução do método de criação de produto!", e.getCause());
@@ -46,31 +32,26 @@ public class ProdutosController {
     }
 
     @PutMapping("alterar")
-    public String alterar() {
-        return "Produto alterado com sucesso";
+    public ResponseEntity<?> alterar(@RequestBody ProdutoAtualizarRequestDto dto) {
+        try {
+            var produto = produtoMapper.toProdutoAtualizar(dto);
+            if (produtoRepository.atualizar(produto))
+                return ResponseEntity.status(HttpStatus.OK).body("Produto '" + produto.getNome() + "' atualizado com sucesso!");
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto '" + produto.getNome() + "' não encontrado!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new CreateProdutoException("Erro na execução do método de criação de produto!", e.getCause());
+        }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<?> excluir(@PathVariable Integer id) {
         try {
-            var query = """
-                    update produtos
-                    set ativo = 0, data_exclusao = current_timestamp
-                    where id = ?
-                      and ativo = 1;
-                """;
-
-            try (var connection = connectionFactory.getConnection()) {
-                var prepareStatement = connection.prepareStatement(query);
-                prepareStatement.setInt(1, id);
-
-                int linhasAfetadas = prepareStatement.executeUpdate();
-
-                if (linhasAfetadas > 0)
-                    return ResponseEntity.status(HttpStatus.OK).body("Produto com id: '" + id + "', foi inativado com sucesso!");
-                else
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum produto foi inativado!");
-            }
+            if (produtoRepository.excluir(id))
+                return ResponseEntity.status(HttpStatus.OK).body("Produto com id: '" + id + "', foi inativado com sucesso!");
+            else
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum produto foi inativado!");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
@@ -78,8 +59,45 @@ public class ProdutosController {
     }
 
     @GetMapping("listar")
-    public String listar() {
-        return "Produto consultado com sucesso";
+    public ResponseEntity<?> listar() {
+        try {
+            var listaProdutos = produtoRepository.listar();
+            if (listaProdutos == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum produto cadastrado!");
+
+            return ResponseEntity.status(HttpStatus.OK).body(produtoMapper.toDtoResponse(listaProdutos));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @GetMapping("buscarPorId/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+        try {
+            var produto = produtoMapper.toDtoResponse(produtoRepository.buscarPorId(id));
+            if (produto == null)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum produto foi encontrado!");
+
+            return ResponseEntity.status(HttpStatus.OK).body(produto);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @GetMapping("buscarPorNome/{nome}")
+    public ResponseEntity<?> buscarPorNome(@PathVariable String nome) {
+        try {
+            var produtos = produtoMapper.toDtoResponse(produtoRepository.buscarPorNome(nome));
+            if (produtos.isEmpty())
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum produto foi encontrado!");
+
+            return ResponseEntity.status(HttpStatus.OK).body(produtos);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
 }
